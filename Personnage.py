fc77bd34ee5009cc7075f544ca6943c1cd822835
AndreCLAVERIA <andreclaveria2002@gmail.com>
2021-05-22 22:00:22 +0200

    personnage

---
+++ Personnage.py
@@ -0,0 +1,139 @@
+class Bernard():
+    def __init__(self, DEF, Attack, Synopsis):
+        self.Synopsis = Synopsis
+        # Entre 1 et 10
+        self.Attack = Attack
+        # entre 1 et 100
+        self.DEF = DEF
+
+    def DEF(self):
+        return self.DEF
+
+    def Attack(self):
+        return self.Attack
+
+    def degat_subit(self, degat_subit):
+        self.DEF -= degat_subit
+        print("Bernard à subit", degat_subit, "dégat")
+
+class Jacquie():
+    def __init__(self, DEF, Attack, Synopsis):
+        self.Synopsis = Synopsis
+        # Entre 1 et 10
+        self.Attack = Attack
+        # entre 1 et 100
+        self.DEF = DEF
+
+    def DEF(self):
+        return self.DEF
+
+    def Attack(self):
+        return self.Attack
+
+    def degat_subit(self, degat_subit):
+        self.DEF -= degat_subit
+        print("Jacquie à subit", degat_subit, "dégat")
+
+
+class Louis():
+    def __init__(self, DEF, Attack, Synopsis):
+        self.Synopsis = Synopsis
+        # Entre 1 et 10
+        self.Attack = Attack
+        # entre 1 et 100
+        self.DEF = DEF
+
+    def DEF(self):
+        return self.DEF
+
+    def Attack(self):
+        return self.Attack
+
+    def degat_subit(self, degat_subit):
+        self.DEF -= degat_subit
+        print("Louis à subit", degat_subit, "dégat")
+
+
+
+class Vlad():
+    def __init__(self, DEF, Attack, Synopsis):
+        self.Synopsis = Synopsis
+        # Entre 1 et 10
+        self.Attack = Attack
+        # entre 1 et 100
+        self.DEF = DEF
+
+    def DEF(self):
+        return self.DEF
+
+    def Attack(self):
+        return self.Attack
+
+    def degat_subit(self, degat_subit):
+        self.DEF -= degat_subit
+        print("Vlad à subit", degat_subit, "dégat")
+
+
+
+class Erica():
+    def __init__(self, DEF, Attack, Synopsis):
+        self.Synopsis = Synopsis
+        # Entre 1 et 10
+        self.Attack = Attack
+        # entre 1 et 100
+        self.DEF = DEF
+
+    def DEF(self):
+        return self.DEF
+
+    def Attack(self):
+        return self.Attack
+
+    def degat_subit(self, degat_subit):
+        self.DEF -= degat_subit
+        print("Erica à subit", degat_subit, "dégat")
+
+
+
+class Stella():
+    def __init__(self, DEF, Attack, Synopsis):
+        self.Synopsis = Synopsis
+        # Entre 1 et 10
+        self.Attack = Attack
+        # entre 1 et 100
+        self.DEF = DEF
+
+    def DEF(self):
+        return self.DEF
+
+    def Attack(self):
+        return self.Attack
+
+    def degat_subit(self, degat_subit):
+        self.DEF -= degat_subit
+        print("Stella à subit", degat_subit, "dégat")
+
+
+
+class Enfant():
+    def __init__(self, DEF, Attack, Synopsis):
+        self.Synopsis = Synopsis
+        # Entre 1 et 10
+        self.DEF = DEF
+        # entre 1 et 100
+        self.Attack = Attack
+
+    def DEF(self):
+        return self.DEF
+
+    def Attack(self):
+        return self.Attack
+
+    def degat_subit(self, degat_subit):
+        self.DEF -= degat_subit
+        print("Mathéo à subit", degat_subit, "dégat")
+
+
+
+
+
